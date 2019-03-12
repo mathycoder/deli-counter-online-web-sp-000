cdf5eb6c36fecc842747b62d5bc8749baa6e965a
Adam S <adam.sarli@gmail.com>
2019-03-12 17:53:41 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,5 +2,11 @@
 def line(current_line)
   if current_line.length == 0 
     puts "The line is currently empty."
+  else 
+    line_string = "The line is currently: "
+    index = 0 
+    while index < current_line.length - 1 do 
+      
+    end 
   end 
 end 
