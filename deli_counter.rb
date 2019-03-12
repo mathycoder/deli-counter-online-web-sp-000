b7029539ae5e9ebc80df1b3213dde1b296489878
Adam S <adam.sarli@gmail.com>
2019-03-12 17:55:57 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,10 +3,10 @@ def line(current_line)
   if current_line.length == 0 
     puts "The line is currently empty."
   else 
-    line_string = "The line is currently: "
+    line_string = "The line is currently:"
     index = 0 
     while index < current_line.length - 1 do 
-      line_string << "#{index+1}. #{current_line[index]}"
+      line_string << " #{index+1}. #{current_line[index]}"
       index+=1 
     end 
     puts line_string 
